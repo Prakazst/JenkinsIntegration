0be9bae23abf7468e9eb003afead711ed0cec9f7
Prakazst <124021013+Prakazst@users.noreply.github.com>
2023-02-07 10:29:25 +0530

    Update Demo.java

--- src/main/java/OraniumTech/SampleProject/Demo.java
+++ src/main/java/OraniumTech/SampleProject/Demo.java
@@ -6,16 +6,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Demo {
 	
  public static void main(String[] args) throws InterruptedException {
-	
-
-	
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com");
 		driver.manage().window().maximize();
 		Thread.sleep(3000);
-		driver.close();
-
-
+		//driver.close();
 	}
 
 }
